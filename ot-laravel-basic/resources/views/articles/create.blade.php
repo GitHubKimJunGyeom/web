a7f8d8848d94affde0661e714f0167f9a91d2c00
김준겸 <>
2024-10-24 21:04:05 +0900

    10.에러메세지 표시

--- ot-laravel-basic/resources/views/articles/create.blade.php
+++ ot-laravel-basic/resources/views/articles/create.blade.php
@@ -13,7 +13,27 @@
                 <input type="hidden" name="_token" value="<?php echo csrf_token();?>">-->
             @csrf
             <input type="text" class="block w-full mb-2 rounded">
+            
+            <!--바디에 에러가 있으면 여기를 표시-->
+            @error('body')
+                <p class="text-xs text-red-500 mb-3" >{{ $message }}</p>
+            @enderror
             <button class="py-1 px-3 bg-black text-white rounded text-xs">Save</button>
         </form>
+        <!--배열 형태로 반환-->
+        {{ dd($errors->all()) }}
+        <!--불값을 반환-->
+        {{ dd($errors->any()) }}
+
+        <?php if($errors->any()): ?>
+        {{ dd($errors->any()) }}
+        <?php endif ?>
+        <!--특정 필드의 첫번쨰 에러만 반환-->
+        {{ dd($errors->first('body')) }}
+        
+        <!--올은 다 가져오지만 겟은 특정 필드지정한것만 가져옴-->
+        {{ dd($errors->get('body')) }}
+        {{-- <?php echo dd($error); ?> --}}
+
     </body>
 </html>
